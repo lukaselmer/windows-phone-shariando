74e92d738ecb76bb8b2504311d85ce0f3c7780fc
Lukas Elmer <lukas.elmer@gmail.com>
2012-02-02 15:22:11 +0000

    windows phone shariando

--- Shariando.Gui.WP7/Properties/AssemblyInfo.cs
+++ Shariando.Gui.WP7/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shariando")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Shariando CH")]
+[assembly: AssemblyDescription("Garantiert Cashback!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Renuo GmbH")]
-[assembly: AssemblyProduct("Shariando.Gui.WP7")]
+[assembly: AssemblyProduct("Shariando CH")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
